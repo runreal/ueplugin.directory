51ae3569c61a8e82c4f7e3e4d67409ca27de196c
Stephen Lacy <me@slacy.me>
2025-05-05 17:35:21 -0700

    feat: add more plugins

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { inngest } from "@/inngest/client"
 import { serve } from "inngest/next"
-import { processPlugins } from "./functions"
+import { processPlugin, processPlugins } from "./functions"
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
 	client: inngest,
-	functions: [processPlugins],
+	functions: [processPlugins, processPlugin],
 })

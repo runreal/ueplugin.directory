9328bb4691c4f321c5c3fa8e1d655adf9980495c
Thibault Le Ouay <thibaultleouay@gmail.Com>
2025-05-15 21:20:59 +0200

    Revert "chore: update design (#4)" (#5)
    This reverts commit b276182480fce6e83784d54d929b13ec13577e25.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
-import { FancyButton } from "@/components/fancy-button";
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
-import { Hero } from "@/components/hero";
-import { Pagination } from "@/components/pagination";
-import { PluginGrid } from "@/components/plugin-grid";
-import { Topbar } from "@/components/topbar";
-import { trpc } from "@/trpc/server";
-import { PlusIcon } from "lucide-react";
-import type { Metadata } from "next";
-import Image from "next/image";
-
-
-
-export const metadata: Metadata = {
-  title: 'The Unreal Plugin Registry',
-  description: 'Discover, share, and contribute to open-source Unreal Engine plugins',
-}
-
+import { FancyButton } from "@/components/fancy-button"
+import { Footer } from "@/components/footer"
+import { Hero } from "@/components/hero"
+import { Pagination } from "@/components/pagination"
+import { PluginGrid } from "@/components/plugin-grid"
+import { Topbar } from "@/components/topbar"
+import { trpc } from "@/trpc/server"
+import { PlusIcon } from "lucide-react"
+import Image from "next/image"
 
 export default async function Home({
 	searchParams,
@@ -35,22 +25,23 @@ export default async function Home({
 			<Topbar showSearch={false} />
 			<Hero />
 
-      <div className="flex flex-col gap-4 max-w-[1200px] w-full m-h-fulr m-auto mt-8 px-8">
-        <div className="flex items-center justify-between mt-8">
-          <div className="text-xl font-medium font-mono">
-            {search ? <span>Seach Results</span> : <span>Popular Plugins</span>}
-          </div>
-          <a href="https://github.com/runreal/plugin.registry/issues/new?template=request_plugin.yml" target="_blank" rel="noreferrer">
-          <FancyButton
-            size="large"
-            className="uppercase font-mono text-gray-200"
-            icon={() => <PlusIcon className="h-5 w-5 ml-2 mb-[2px]" />}
-          >
-            Submit
-          </FancyButton>
-          </a>
-        </div>
-        <PluginGrid plugins={data} />
+			<div className="flex flex-col gap-4 max-w-[1200px] w-full m-h-fulr m-auto mt-8 px-8">
+				<div className="flex items-center justify-between mt-8">
+					<div className="text-xl font-medium">
+						{search ? <span>Seach Results</span> : <span>Popular Plugins</span>}
+					</div>
+					<a
+						href="https://github.com/runreal/plugin.registry/issues/new?template=request_plugin.yml"
+						target="_blank"
+						rel="noreferrer"
+						className="no-underline!"
+					>
+						<FancyButton size="large" className="uppercase" icon={() => <PlusIcon className="h-5 w-5 ml-2 mb-[2px]" />}>
+							Submit
+						</FancyButton>
+					</a>
+				</div>
+				<PluginGrid plugins={data} />
 
 				{data.length >= 99 || offset > 1 ? (
 					<Pagination search={search} offset={offset} count={data.length} className="mt-8" />

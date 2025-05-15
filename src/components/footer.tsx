4091c20f1a305fbcae972e37da31d03aa702dd65
Thibault Le Ouay <thibaultleouay@gmail.Com>
2025-05-15 21:20:31 +0200

    Revert "chore: update design (#4)"
    This reverts commit b276182480fce6e83784d54d929b13ec13577e25.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 export function Footer({ className }: { className?: string }) {
 	return (
 		<div className={cn("flex w-full flex-col p-8 border-t border-dashed mt-auto", className)}>
-			<div className="w-full m-auto text-sm text-center font-mono">
+			<div className="w-full m-auto text-sm text-center">
 				Powered by{" "}
 				<a href="https://runreal.dev" target="_blank" rel="noopener noreferrer">
 					runreal.dev
